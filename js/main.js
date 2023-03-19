a03d62c973a3fc3008657ede916ce5ecc381b8cb
Ян Коваленко <kovalenko.yai@edu.sbpstu.ru>
2023-03-19 15:33:07 +0300

    rand изменен с let на const

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 function getRandomInt(min, max) {
 
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
 function checkStringLength(str, maxLength) {
   if (str.length <= maxLength) {
     return true;
-  } else return false;
+  } else {
+    return false;
+  }
 }
 
